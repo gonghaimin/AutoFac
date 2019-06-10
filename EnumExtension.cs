1b3acfd06b87f79059a97ccadd44eb06dd5f72e7
gonghaimin <2535355994@qq.com>
2019-06-10 22:04:58 +0800

    22

---
+++ EnumExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Reflection;
+using System.Monads;
+
+namespace NextPms.Util.Extensions
+{
+    public static class EnumExtension
+    {
+        private static readonly ConcurrentDictionary<string, string> Cache = new ConcurrentDictionary<string, string>();
+
+        public static string OfDescription<T>(this T value) where T : struct, IConvertible
+        {
+            var cacheKey = typeof (T).FullName + value.ToString();
+
+            var result = Cache.GetOrAdd(cacheKey, key =>
+            {
+                var enumType = value.GetType();
+
+                return
+                    Enum.GetName(enumType, value)
+                        .If(name => !string.IsNullOrEmpty(name))
+                        .With(name => enumType.GetField(name))
+                        .With(field =>
+                             Attribute.GetCustomAttribute(field, typeof (DescriptionAttribute), false) as
+                                    DescriptionAttribute)
+                        .With(descriptionAttribute => descriptionAttribute.Description);
+            });
+
+            return result;
+        }
+    }
+}
